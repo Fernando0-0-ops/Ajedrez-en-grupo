b9df130d3ef274cee276439ad281a7a4242fe44b
Julián V <jjulianvasquez14@gmail.com>
2026-05-18 23:38:48 -0600

    Agregar archivos de proyecto.

---
+++ Ajed01/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("================================");
+		Console.WriteLine("   SISTEMA DE AJEDREZ");
+		Console.WriteLine("================================");
+
+		Console.WriteLine("\nBienvenido al sistema");
+
+		Console.ReadLine();
+	}
+}
