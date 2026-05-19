732020edbfdf47e2aadf329eb8bc2f82c947ea7a
JoseV502 <jjulianvasquez14@gmail.com>
2026-05-19 06:29:09 -0600

    Agregar archivos de proyecto.
    Aún más

---
+++ Ajed03.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+	static void Main(string[] args)
+	{
+		string usuarioCorrecto = "admin";
+		string contraseñaCorrecta = "Admin123!";
+
+		string usuarioIngresado;
+		string contraseñaIngresada;
+
+		Console.WriteLine("================================");
+		Console.WriteLine("   SISTEMA DE AJEDREZ");
+		Console.WriteLine("================================");
+
+		Console.WriteLine("\nBienvenido al sistema");
+
+		Console.Write("\nIngrese el usuario: ");
+		usuarioIngresado = Console.ReadLine();
+
+		Console.Write("Ingrese la contraseña: ");
+		contraseñaIngresada = Console.ReadLine();
+
+		if (usuarioIngresado == usuarioCorrecto &&
+			contraseñaIngresada == contraseñaCorrecta)
+		{
+			Console.WriteLine("\nAcceso permitido");
+		}
+		else
+		{
+			Console.WriteLine("\nUsuario o contraseña incorrectos");
+		}
+	}
+}
