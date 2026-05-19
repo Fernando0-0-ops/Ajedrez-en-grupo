21f6e2e4c7477e5ab224ac408353bc26995e6973
Fernandiho O__O <ari.pretzantzin@gmail.com>
2026-05-18 23:12:40 -0600

    Crear SistemaPuntaje

--- Tablero de ajedrez/Program.cs
+++ Tablero de ajedrez/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-//Clase Jugador y Pieza//
-class Jugador//
+
+class Jugador
 {
     public string nombre;
     public string color;
@@ -11,4 +11,62 @@ class Pieza
 {
     public string tipo;
     public string jugador;
+}
+
+class SistemaPuntaje
+{
+    public int puntajeMaximo = 0;
+    public string mejorJugador = "";
+
+    // CALCULAR PUNTOS
+    //Función que recibe un tipo de pieza y devuelve puntos. devuelve un int 
+    public int CalcularPuntaje(string tipo)
+    {
+        if (tipo == "Soldado")
+        {
+            return 10;
+        }
+
+        else if (tipo == "Torre")
+        {
+            return 10;
+        }
+
+        else if (tipo == "Rey")
+        {
+            return 60;
+        }
+
+        return 0;
+    }
+
+    // GUARDAR RECORD
+    //Función para guardar el récord.
+    public void GuardarRecord(Jugador ganador)
+    {
+        //ganador pasa a representar al jugador que ganó la partida, y se compara su puntaje con el puntaje máximo registrado.
+        //Si el puntaje del ganador es mayor que el puntaje máximo, se actualiza el puntaje máximo y se guarda el nombre del mejor jugador.
+        if (ganador.puntaje > puntajeMaximo)
+        {
+            //guardamos el nuevo puntaje y nombre
+            puntajeMaximo = ganador.puntaje;
+            mejorJugador = ganador.nombre;
+        }
+    }
+
+    // MOSTRAR RECORD
+    public void VerPuntajeAlto()
+    {
+        if (puntajeMaximo == 0)
+        {
+            Console.WriteLine("No hay puntajes guardados");
+        }
+
+        else
+        {
+            Console.WriteLine("\n-----Puntaje Más Alto-----");
+            Console.WriteLine("Jugador: " + mejorJugador);
+            Console.WriteLine("Puntaje: " + puntajeMaximo);
+        }
+    }
 }
