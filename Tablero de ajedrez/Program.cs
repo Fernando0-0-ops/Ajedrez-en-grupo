d33cf8b7822f10e19231abe6f3e20424b43a5867
Fernandiho O__O <ari.pretzantzin@gmail.com>
2026-05-18 22:13:38 -0600

    Agregar archivos de proyecto.

---
+++ Tablero de ajedrez/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Jugador
+{
+    public string nombre;
+    public string color;
+    public int puntaje;
+}
+
+class Pieza
+{
+    public string tipo;
+    public string jugador;
+}
