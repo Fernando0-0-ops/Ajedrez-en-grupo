77a500795878119017887a18346e051d4aa15a41
Fernandiho O__O <ari.pretzantzin@gmail.com>
2026-05-21 17:15:31 -0600

    Agregar archivos de proyecto.

---
+++ Parte de pablo/Program.cs
@@ -0,0 +1,25 @@
+﻿public bool MovimientoValido(int fo, int co, int fd, int cd)
+{
+    if (fo < 0 || fo > 7 || co < 0 || co > 7 ||
+        fd < 0 || fd > 7 || cd < 0 || cd > 7)
+        return false;
+
+    Pieza p = t.tablero[fo, co];
+
+    if (p == null || p.jugador != turnoActual)
+        return false;
+
+    Pieza destino = t.tablero[fd, cd];
+
+    if (destino != null && destino.jugador == turnoActual)
+        return false;
+
+    return ValidarMovimiento(p.tipo, p.jugador, fo, co, fd, cd);
+}
+
+public void CambiarTurno()
+{
+    turnoActual = (turnoActual == "Blanco")
+        ? "Negro"
+        : "Blanco";
+}
